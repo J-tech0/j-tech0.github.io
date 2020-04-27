42f38335db3ed2b15b0b6d657524db30d913a895
J-tech0 <56737975+J-tech0@users.noreply.github.com>
2020-04-27 13:02:43 +0100

    new content and images and cache bust

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const options = {
   linkSelector:
     'a[href^="' +
     window.location.origin +
-    '"]:not([data-no-swup]), a[href^="/"]:not([data-no-swup])'
+    '"]:not([data-no-swup]), a[href^="/"]:not([data-no-swup])',
+    plugins: [new SwupScrollPlugin({animateScroll: false})]
 };
 const swup = new Swup(options); // only this line when included with script tag
