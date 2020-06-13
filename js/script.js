dd03942ed2c7d90d56bd99331e021d3bde88a92d
J-tech0 <56737975+J-tech0@users.noreply.github.com>
2020-06-13 15:24:57 +0100

    fixed a huge bug with the our services page, added the newsroom page

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-const swupoptions = {
+const options = {
   linkSelector:
     'a[href^="' +
     window.location.origin +
-    '"]:not([data-no-swup]), a[href^="/"]:not([data-no-swup])',
-    plugins: [new SwupScrollPlugin({animateScroll: false}), new SwupScriptsPlugin({optin: true})]
+    '"]:not([data-no-swup]), a[href^="/"]:not([data-no-swup]), a[href^="#"]:not([data-no-swup])',
+    plugins: [new SwupScrollPlugin({animateScroll: false, doScrollingRightAway: false}), new SwupDebugPlugin()]
 };
-const swup = new Swup(swupoptions);
+const swup = new Swup(options);
