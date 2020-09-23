679f2cb2420c915be6df1d3b7981c3b196f18e71
J-tech0 <56737975+J-tech0@users.noreply.github.com>
2020-09-23 13:15:08 +0100

    fix honeypot email with swup

--- js/script.js
+++ js/script.js
@@ -5,4 +5,18 @@ const options = {
     '"]:not([data-no-swup]), a[href^="/"]:not([data-no-swup]), a[href^="#"]:not([data-no-swup])',
     plugins: [new SwupScrollPlugin({animateScroll: false, doScrollingRightAway: false}), new SwupDebugPlugin()]
 };
+
 const swup = new Swup(options);
+
+function init() { //all the functions that could be run on page load
+    if (document.querySelector('#infoemail')) { //honeypot info email
+        console.log("spam email swapped");
+        var asdf="info", ghjk="worksgroup", lzxc="synergy";
+        document.getElementById("infoemail").innerHTML = '<a href="mailto:' + asdf + '@' + lzxc + ghjk + '.com' + '">'+ asdf + '@' + lzxc + ghjk + '.com' + '</a>';
+    }
+
+}
+
+init(); //runs any init code once when loaded
+
+swup.on('contentReplaced', init); //runs any init code whenever navigation takes place
